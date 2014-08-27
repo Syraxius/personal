acba9af1365f07a851a283d80c4420fe803a1ee2
Syraxius <syraxius@hotmail.com>
2014-08-27 22:08:16 +0800

    TextBuddy CE1 - Added Documentation
    Added basic documentation and added some warning prints for
    TextBuddyLogic.

--- TextBuddy/src/ce1/TextBuddy.java
+++ TextBuddy/src/ce1/TextBuddy.java
@@ -1,9 +1,17 @@
 package ce1;
 
+/**
+ * This program instantiates and run TextBuddy Text UI.
+ *
+ * The program requires a valid NTFS file name to be
+ * supplied as the argument.
+ *
+ * @author Ang Kah Min, Kelvin
+ */
 public class TextBuddy {
 
-    public static void main(String[] args) {
-	TextBuddyTUI tb = new TextBuddyTUI(args);
-	tb.systemRun();
-    }
+	public static void main(String[] args) {
+		TextBuddyTUI tb = new TextBuddyTUI(args);
+		tb.systemRun();
+	}
 }
--- TextBuddy/src/ce1/TextBuddyLogic.java
+++ TextBuddy/src/ce1/TextBuddyLogic.java
@@ -7,118 +7,136 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 
+/**
+ * This program handles the logic for the TextBuddy system.
+ * 
+ * This implementation uses an ArrayList<String> as the data structure and is
+ * able to perform add, delete, clear and save functionalities.
+ * 
+ * The text file is only used as data storage and is written to disk by
+ * invoking the save() method.
+ * 
+ * @author Ang Kah Min, Kelvin
+ */
 public class TextBuddyLogic {
 
-    // Objects Declaration
+	// Warning Strings
+	
+	private static final String WARNING_READ = "Warning: Unable to read from file.";
+	private static final String WARNING_WRITE = "Warning: Unable to create new file.";
+	
+	// Objects Declaration
+	
+	Path path;
+	List<String> list;
 
-    Path path;
-    List<String> list;
+	// Constructors
 
-    // Constructors
-
-    public TextBuddyLogic(String filePath) {
-	initializePath(filePath);
-	initializeFile();
-	initializeList();
-    }
-
-    // Initialization Methods
-
-    private void initializePath(String filePath) {
-	this.path = Paths.get(filePath);
-    }
-
-    private void initializeFile() {
-	boolean isExistingFile = Files.exists(path);
-	if (!isExistingFile) {
-	    try {
-		//This would write a blank file.
-		Files.write(path, new byte[0], StandardOpenOption.CREATE,
-			    StandardOpenOption.TRUNCATE_EXISTING);
-	    } catch (IOException e) {
-	    }
+	public TextBuddyLogic(String fileName) {
+		initializePath(fileName);
+		initializeFile();
+		initializeList();
 	}
-    }
 
-    private void initializeList() {
-	try {
-	    list = Files.readAllLines(path);
-	} catch (Exception e) {
+	// Initialization Methods
+
+	private void initializePath(String fileName) {
+		this.path = Paths.get(fileName);
 	}
-    }
 
-    // Accessor Methods
-
-    public String getFilePath() {
-	return this.path.toString();
-    }
-
-    public String getFileName() {
-	return this.path.getFileName().toString();
-    }
-
-    public List<String> getList() {
-	return this.list;
-    }
-
-    public int getListSize() {
-	return this.list.size();
-    }
-
-    // Verification Methods
-
-    public boolean isFileReadableWritable() {
-	return Files.isReadable(path) && Files.isWritable(path);
-    }
-
-    // Operation Methods
-
-    public boolean add(String content) {
-	boolean success;
-	try {
-	    boolean isNull = content == null;
-	    boolean isZeroLength = content.length() == 0;
-	    if (isNull || isZeroLength) {
-		throw new NullPointerException();
-	    }
-	    list.add(content);
-	    success = true;
-	} catch (Exception e) {
-	    success = false;
+	private void initializeFile() {
+		boolean isExistingFile = Files.exists(path);
+		if (!isExistingFile) {
+			try {
+				// This would write a blank file if it does not exist.
+				Files.write(path, new byte[0], StandardOpenOption.CREATE,
+						StandardOpenOption.TRUNCATE_EXISTING);
+			} catch (IOException e) {
+				System.out.println(WARNING_WRITE);
+			}
+		}
 	}
-	return success;
-    }
 
-    public String delete(int lineNumber) {
-	String deletedLine;
-	try {
-	    deletedLine = list.remove(lineNumber);
-	} catch (Exception e) {
-	    deletedLine = null;
+	private void initializeList() {
+		try {
+			list = Files.readAllLines(path);
+		} catch (Exception e) {
+			System.out.println(WARNING_READ);
+		}
 	}
-	return deletedLine;
-    }
 
-    public boolean clear() {
-	boolean success;
-	try {
-	    list.clear();
-	    success = true;
-	} catch (Exception e) {
-	    success = false;
-	}
-	return success;
-    }
+	// Accessor Methods
 
-    public boolean save() {
-	boolean success;
-	try {
-	    Files.write(path, list, StandardOpenOption.CREATE,
-		    	StandardOpenOption.TRUNCATE_EXISTING);
-	    success = true;
-	} catch (Exception e) {
-	    success = false;
+	public String getFilePath() {
+		return this.path.toString();
 	}
-	return success;
-    }
+
+	public String getFileName() {
+		return this.path.getFileName().toString();
+	}
+
+	public List<String> getList() {
+		return this.list;
+	}
+
+	public int getListSize() {
+		return this.list.size();
+	}
+
+	// Verification Methods
+
+	public boolean isFileReadableWritable() {
+		return Files.isReadable(path) && Files.isWritable(path);
+	}
+
+	// Operation Methods
+
+	public boolean add(String content) {
+		boolean success;
+		try {
+			boolean isNull = content == null;
+			boolean isZeroLength = content.length() == 0;
+			if (isNull || isZeroLength) {
+				throw new NullPointerException();
+			}
+			list.add(content);
+			success = true;
+		} catch (Exception e) {
+			success = false;
+		}
+		return success;
+	}
+
+	public String delete(int lineNumber) {
+		String deletedLine;
+		try {
+			deletedLine = list.remove(lineNumber);
+		} catch (Exception e) {
+			deletedLine = null;
+		}
+		return deletedLine;
+	}
+
+	public boolean clear() {
+		boolean success;
+		try {
+			list.clear();
+			success = true;
+		} catch (Exception e) {
+			success = false;
+		}
+		return success;
+	}
+
+	public boolean save() {
+		boolean success;
+		try {
+			Files.write(path, list, StandardOpenOption.CREATE,
+					StandardOpenOption.TRUNCATE_EXISTING);
+			success = true;
+		} catch (Exception e) {
+			success = false;
+		}
+		return success;
+	}
 }
