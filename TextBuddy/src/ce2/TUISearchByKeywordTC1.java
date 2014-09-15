22bd883d9b0b4d07b35b5e8ed7460f7eb36af804
Syraxius <syraxius@hotmail.com>
2014-09-15 22:36:50 +0800

    Added TUISearchByKeywordTC1
    Added a basic search for one matching entry.

---
+++ TextBuddy/src/ce2/TUISearchByKeywordTC1.java
@@ -0,0 +1,32 @@
+package ce2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TUISearchByKeywordTC1 {
+	
+	TextBuddyTUI tui;
+
+	@Before
+	public void setUp() throws Exception {
+		String[] args = {"test.txt"};
+		tui = new TextBuddyTUI(args);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		tui.processUserCommand("add carrot");
+		tui.processUserCommand("add banana");
+		tui.processUserCommand("add apple");
+		assertEquals("- apple",tui.processUserCommand("search apple"));
+		tui.processUserCommand("clear");
+	}
+
+}
