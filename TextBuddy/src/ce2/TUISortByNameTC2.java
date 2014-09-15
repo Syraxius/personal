f4fb92ffb3107b39c7944f88033448f37482ac95
Syraxius <syraxius@hotmail.com>
2014-09-15 22:28:55 +0800

    Added TUISortByNameTC2
    This is to test for whether sorting works.

---
+++ TextBuddy/src/ce2/TUISortByNameTC2.java
@@ -0,0 +1,33 @@
+package ce2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TUISortByNameTC2 {
+	
+	TextBuddyTUI tui;
+
+	@Before
+	public void setUp() throws Exception {
+		String[] args = {"test.txt"};
+		tui = new TextBuddyTUI(args);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		tui.processUserCommand("add c");
+		tui.processUserCommand("add b");
+		tui.processUserCommand("add a");
+		tui.processUserCommand("sort");
+		assertEquals("1. a\n2. b\n3. c\n",tui.processUserCommand("display"));
+		tui.processUserCommand("clear");
+	}
+
+}
