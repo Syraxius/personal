56059cabc2de66f9cfab73acd6335ed010addfc8
Syraxius <syraxius@hotmail.com>
2014-09-15 21:18:02 +0800

    Renamed Test Cases for Logic
    Renamed the test cases so that it can be differentiated between Logic
    and TUI cases.

---
+++ TextBuddy/src/ce2/LogicSortByNameTC1.java
@@ -0,0 +1,32 @@
+package ce2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LogicSortByNameTC1 {
+	
+	TextBuddyLogic logic;
+
+	@Before
+	public void setUp() throws Exception {
+		logic = new TextBuddyLogic("test.txt");
+		logic.addLine("c");
+		logic.addLine("b");
+		logic.addLine("a");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		logic.clearAllLines();
+	}
+
+	@Test
+	public void test() {
+		boolean sortSuccess = logic.sort();
+		assertTrue(sortSuccess);
+	}
+
+}
