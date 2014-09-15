c4d51f43e61efb9e3fe909fd802f25cd5736cc5b
Syraxius <syraxius@hotmail.com>
2014-09-15 22:25:15 +0800

    Added TUISortByNameTC1
    This time in JUnit. Made sure that it shows the correct sort message.

---
+++ TextBuddy/src/ce2/TUISortByNameTC1.java
@@ -0,0 +1,32 @@
+package ce2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TUISortByNameTC1 {
+	
+	TextBuddyTUI tui;
+
+	@Before
+	public void setUp() throws Exception {
+		String[] args = {"test.txt"};
+		tui = new TextBuddyTUI(args);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals("added to test.txt: \"c\"", tui.processUserCommand("add c"));
+		assertEquals("added to test.txt: \"b\"", tui.processUserCommand("add b"));
+		assertEquals("added to test.txt: \"a\"", tui.processUserCommand("add a"));
+		assertEquals("test.txt has been sorted", tui.processUserCommand("sort"));		
+		assertEquals("all content deleted from test.txt", tui.processUserCommand("clear"));
+	}
+
+}
