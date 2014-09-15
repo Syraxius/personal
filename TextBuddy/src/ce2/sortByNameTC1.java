99daca4ed7f5236c3ffe22605b3e7561bee07524
Syraxius <syraxius@hotmail.com>
2014-09-15 21:06:12 +0800

    Added sortByNameTC1
    This tests for whether sort() returns true.

--- TextBuddy/src/ce2/sortByNameTC1.java
+++ TextBuddy/src/ce2/sortByNameTC1.java
@@ -1,24 +1,34 @@
 package ce2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class sortByNameTC1 {
+	
+	TextBuddyLogic logic;
 
 	@Before
 	public void setUp() throws Exception {
+		logic = new TextBuddyLogic("test.txt");
+		logic.addLine("c");
+		logic.addLine("b");
+		logic.addLine("a");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		logic.clearAllLines();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		boolean sortSuccess = logic.sort();
+		assertTrue(sortSuccess);
 	}
 
 }
